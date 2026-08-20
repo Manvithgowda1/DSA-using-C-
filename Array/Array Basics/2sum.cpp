d68e22dd47a4c33c0bf155ce44228e9e8b16b7ad
Manvithgowda1 <manvithgowdaa1315@gmail.com>
2026-08-20 12:35:18 +0000

    2sum using hashing

--- Array/Array Basics/2sum.cpp
+++ Array/Array Basics/2sum.cpp
@@ -1,25 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> hash;
-        int n=nums.size();
-        int temp;
-        for(int i=0;i<n;i++){
-            temp=target-nums[i];
-            if(hash.find(temp)!=hash.end()){
-                return {hash[temp],i};
-            }
-            hash[nums[i]]=i;
+        unordered_map<int, int> hash;
 
+        for (int i = 0; i < nums.size(); i++) {
+            int temp = target - nums[i];
+
+            if (hash.find(temp) != hash.end()) {
+                return {hash[temp], i};
+            }
+
+            hash[nums[i]] = i;
         }
+
         return {};
-    
     }
 };
 
+int main() {
+    Solution solution;
+
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "Enter the target: ";
+    cin >> target;
+
+    vector<int> result = solution.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << " " << result[1] << endl;
+    } else {
+        cout << "No two numbers found." << endl;
+    }
+
+    return 0;
+}
+
 //Example
-nums =[2,7,11,15]
-target =9
-output=[0,1]
+// nums =[2,7,11,15]
+// target =9
+// output=[0,1]
 //Time Complexity O(n)
 //space Complexity O(n)
