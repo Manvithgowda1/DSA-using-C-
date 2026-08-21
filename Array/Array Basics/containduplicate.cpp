1ca12a03a011372a8ae160ebcb4758907cbc53cf
Manvithgowda1 <manvithgowdaa1315@gmail.com>
2026-08-21 17:35:57 +0530

    Add containsDuplicate function in Solution class
    Implement a method to check for duplicates in an array.

---
+++ Array/Array Basics/containduplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> s(nums.begin(),nums.end());
+        if(nums.size()==s.size()){
+            return false;
+        }
+        return true;
+        
+    }
+};
+
+// Time complexity o(n log n)
+// space complexity o(n)
+
+input: nums =[1,2,3,1]
+Output: true
