b9b1c5ba78b515ab7240c97666f7c8f3d947a763
Manvithgowda1 <manvithgowdaa1315@gmail.com>
2026-08-22 17:40:31 +0530

    optimal solution for majority elements problem
    Implemented majorityElement function to find the majority element in a vector using a hash map.

--- Array/Array Basics/containduplicate.cpp
+++ Array/Array Basics/containduplicate.cpp
@@ -15,3 +15,30 @@ public:
 
 input: nums =[1,2,3,1]
 Output: true
+
+// optimal
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        // vector<pair<int, int>> vec(m.begin(), m.end());
+        // sort(vec.begin(),vec.end(), [](auto &a, auto &b){
+        //     return a.second>b.second;
+        // });
+        int maxf=0;
+        int ans;
+        for(auto &x:m){
+            if(maxf< x.second){
+                maxf=x.second;
+                ans=x.first;
+            }
+        }
+        return ans;
+    }
+};
+
+// time complexity O(n)
+// space complexity O(n)
