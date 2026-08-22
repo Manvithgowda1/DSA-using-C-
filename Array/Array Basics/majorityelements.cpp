0bfd9d10b62757bbef13c9b603854a62d4206a65
Manvithgowda1 <manvithgowdaa1315@gmail.com>
2026-08-22 17:46:51 +0530

    Add majority element function using hash map
    Implemented majority element algorithm using a hash map to count occurrences.

--- Array/Array Basics/majorityelements.cpp
+++ Array/Array Basics/majorityelements.cpp
@@ -12,3 +12,30 @@ public:
 
 input: nums =[3,2,3]
 Output: 3
+
+// optimal
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i=0;i<nums.size();i++){
+            m[nums[i]]++;
+        }
+        // vector<pair<int, int>> vec(m.begin(), m.end());
+        // sort(vec.begin(),vec.end(), [](auto &a, auto &b){
+        //     return a.second>b.second;
+        // });
+        int maxf=0;
+        int ans;
+        for(auto &x:m){
+            if(maxf< x.second){
+                maxf=x.second;
+                ans=x.first;
+            }
+        }
+        return ans;
+    }
+};
+
+// time complexity O(n)
+// space complexity O(n)
